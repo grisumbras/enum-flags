58408c534ed9c1c56b9f0b15a7fc20fb47f284b9
Дмитрий Архипов <grisumbras@gmail.com>
2014-08-10 18:11:04 +0400

    Added partial container interface (akin to unorered_set).

--- flags.hpp
+++ flags.hpp
@@ -3,8 +3,12 @@
 
 
 #include <initializer_list>
+#include <iterator>
 #include <type_traits>
 #include <utility>
+
+
+template <class E, class Enabler = void> struct flags;
 
 
 namespace detail_ {
@@ -21,11 +25,75 @@ struct is_enum_class<E, typename std::enable_if<std::is_enum<E>::value>::type>
         !std::is_convertible<E, typename std::underlying_type<E>::type>::value>
 {};
 
+template <class E> class FlagsEnumIterator;
+template <class E> inline bool operator==(const FlagsEnumIterator<E> &i1,
+                                          const FlagsEnumIterator<E> &i2);
+template <class E> inline bool operator!=(const FlagsEnumIterator<E> &i1,
+                                          const FlagsEnumIterator<E> &i2);
+
+
+template <class E>
+class FlagsEnumIterator {
+    template <class E1, class V> friend class flags;
+    friend bool operator==<>(const FlagsEnumIterator<E> &i1,
+                             const FlagsEnumIterator<E> &i2);
+    friend bool operator!=<>(const FlagsEnumIterator<E> &i1,
+                             const FlagsEnumIterator<E> &i2);
+
+public:
+    using difference_type = std::ptrdiff_t;
+    using value_type = E;
+    using pointer = value_type*;
+    using reference = const value_type;
+    using iterator_category = std::forward_iterator_tag;
+
+    FlagsEnumIterator() : mask_{0} {}
+
+    FlagsEnumIterator &operator++() {
+        nextMask();
+        return *this;
+    }
+    FlagsEnumIterator operator++(int) {
+        auto copy = *this;
+        ++(*this);
+        return copy;
+    }
+
+    reference operator*() { return static_cast<value_type>(mask_); }
+
+private:
+    using underlying_type = typename flags<E>::underlying_type;
+
+    FlagsEnumIterator(const flags<E> &fl) : mask_{1}
+                                          , uvalue_{fl.underlying_value()} {
+        if (!(mask_ & uvalue_)) { nextMask(); }
+    }
+
+    void nextMask() {
+        do {
+            mask_ <<= 1;
+        } while (mask_ && !(mask_ & uvalue_));
+    }
+
+    underlying_type uvalue_;
+    underlying_type mask_;
+};
+
+
+
+template <class E> bool operator==(const FlagsEnumIterator<E> &i1,
+                                   const FlagsEnumIterator<E> &i2) {
+    return i1.mask_ == i2.mask_;
+}
+
+
+template <class E> bool operator!=(const FlagsEnumIterator<E> &i1,
+                                   const FlagsEnumIterator<E> &i2) {
+    return i1.mask_ != i2.mask_;
+}
+
 
 } // namespace detail_
-
-
-template <class E, class Enabler = void> struct flags;
 
 
 template <class E>
@@ -33,7 +101,18 @@ struct flags<E,
              typename std::enable_if<detail_::is_enum_class<E>::value>::type> {
 public:
     using enum_type = E;
-    using underlying_type = typename std::underlying_type<enum_type>::type;
+    using underlying_type = typename std::make_unsigned<
+        typename std::underlying_type<enum_type>::type>::type;
+
+    using iterator = detail_::FlagsEnumIterator<E>;
+    using const_iterator = iterator;
+    using value_type = typename iterator::value_type;
+    using reference = typename iterator::reference;
+    using const_reference = typename iterator::reference;
+    using pointer = enum_type*;
+    using const_pointer = const enum_type*;
+    using size_type = std::size_t;
+    using difference_type = typename iterator::difference_type;
 
     constexpr flags() : val_{0} {}
 #ifdef ENUM_CLASS_FLAGS_FORBID_IMPLICT_CONVERSION
@@ -95,6 +174,19 @@ public:
 
     constexpr underlying_type underlying_value() const { return val_; }
     void set_underlying_value(underlying_type newval) { val_ = newval; }
+
+    bool empty() const noexcept { return !val_; }
+    constexpr size_type max_size() const noexcept {
+        return sizeof(underlying_type) * 8;
+    }
+
+    iterator begin() { return iterator(*this); }
+    const_iterator begin() const { return const_iterator(*this); }
+    const_iterator cbegin() const { return const_iterator(*this); }
+
+    iterator end() { return iterator(); }
+    const_iterator end() const { return const_iterator(); }
+    const_iterator cend() const { return const_iterator(); }
 
 private:
     constexpr explicit flags(underlying_type val) : val_{val} {}
