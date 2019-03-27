09395d73527bcf18f140a946cc034a9d5f088ab9
Dmitry Arkhipov <grisumbras@gmail.com>
2019-03-27 15:57:12 +0300

    put store-env action in a separate repository

--- .github/store-env/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-for e in $(env); do
-  k=$(echo $e | cut -d= -f1)
-  v=$(echo $e | sed 's/^.*=//')
-  if [ "x$k" = "x${k#GITHUB_}" ]; then
-    echo "export $k='$v'" >>~/.profile
-  fi
-done
